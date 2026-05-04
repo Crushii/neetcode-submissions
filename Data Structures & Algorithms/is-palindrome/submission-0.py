c3b153a24a6d6aecaa9e5ad65366103a6d7beef1
Rushikesh Chapole <rushikeshchapole007@gmail.com>
2026-05-04 21:28:18 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string = ''
+        for i in s:
+            if i.isalpha() or i.isdigit():
+                string += i
+
+        return string.lower() == string.lower()[::-1]
